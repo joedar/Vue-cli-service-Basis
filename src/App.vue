4e86f20da0cf37b11da9c762a46fa40992734439
joedar <joedar@qq.com>
2019-06-25 20:28:00 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="apps">
+		<span>{{ hello }}</span>
+	</div>
+</template>
+<script>
+export default {
+	data () {
+		return {
+			hello : 'hello Vue, I am Joedar !'
+		}
+	}
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	publicPath : './'
+	// 不开启MAP
+	,productionSourceMap : false
+	// 启动设置
+	,devServer : {
+		// 端口号
+		port : 8000
+	}
+};
